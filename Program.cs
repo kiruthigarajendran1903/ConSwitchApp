4621832ee9390e84df06d93e99801e7b44588dc1
kiruthigarajendran1903 <kiruthiga2807@gmail.com>
2023-07-12 17:45:32 +0530

    conditions commit

---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConSwitch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*String cCode;
+            String lang;
+            Console.WriteLine("Enter Country Code");
+            cCode= Console.ReadLine();
+            switch (cCode)
+            {
+                case "uk":
+                case "us":
+                    {
+                        lang = "English";
+                        break;
+                    }
+                case "af":
+                case "paki":
+                    {
+                        lang = "Dari, Pashto";
+                        break;
+                    }
+                case "in":
+                    {
+                        lang = "Hindi, English, Tamil, Telugu, Punjabi";
+                        break;
+                    }
+                case "uae":
+                    {
+                        lang = "Arabic";
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine($"Sorry !! Your country {cCode} is not in ourDb");
+                        lang = "Not Found!!!";
+                        break;
+                    }
+            }
+            Console.WriteLine("Country code" + cCode + "Language(s)" + lang);
+            Console.ReadKey();*/
+            string cCode, lang, choice;
+            do
+            {
+                Console.WriteLine("Enter Country Code");
+                cCode = Console.ReadLine().ToLower();
+                switch (cCode)
+                {
+                    case "uk":
+                    case "us":
+                        {
+                            lang = "English";
+                            break;
+                        }
+                    case "af":
+                        {
+                            lang = "Dari,Pashto";
+                            break;
+                        }
+                    case "in":
+                        {
+                            lang = "Hindi,English,Tamil,Telgu,Punjabi";
+                            break;
+                        }
+                    case "uae":
+                    case "om":
+                    case "ksa":
+                    case "egypt":
+                        {
+                            lang = "Arabic";
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine($"Sorry!! Your country {cCode} is not in OurDb");
+                            lang = "Not Found!!!";
+                            break;
+                        }
+                }
+                Console.WriteLine("Country Code: \t " + cCode + "\n Language(s): \t" + lang);
+                Console.WriteLine("Do you wanna continue if yes press y \n To exit press any key");
+                choice = Console.ReadLine().ToLower();
+            }
+            while (choice == "y");
+
+
+        }
+
+    }
+
+    }
+
